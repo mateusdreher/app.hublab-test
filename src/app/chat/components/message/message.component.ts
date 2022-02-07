e4c2ddf8b159faffbe3f2d5e7aa70f03f74d4449
mateusdreher <mateusdreher.dev@gmail.com>
2022-02-06 21:17:38 -0300

    Correct date label

--- src/app/chat/components/message/message.component.ts
+++ src/app/chat/components/message/message.component.ts
@@ -8,22 +8,29 @@ import { MessageDto } from '../../dtos/message.dto';
 })
 export class MessageComponent implements OnInit {
 
-  @Input() message: MessageDto = {
-    name: '',
-    message: '',
-    send_at: new Date(),
-    room: ''
-  };
+  @Input() message: MessageDto;
   date_label = '';
 
-  constructor() {
-    const diffTime = Math.abs(new Date().getTime() - this.message.send_at.getTime());
-    const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
-    
-    if (diffDays == 0) this.date_label = 'Hoje às '; 
-    if (diffDays == 1) this.date_label = 'Ontem às '; 
+  constructor() {    
   }
 
   ngOnInit(): void {
+  }
+
+  setDateLabel() {
+    const diffDays = this.getDateDiff(new Date(), this.message.send_at);
+
+    if (diffDays == 0) this.date_label = 'Hoje às '; 
+    else if (diffDays == 1) this.date_label = 'Ontem às '; 
+  }
+
+  getDateDiff(date1: Date, date2: Date) {
+    const msPeerDays = 1000 * 60 * 60 * 24;
+    date2 = new Date(date2);
+
+    const dateUTC1 = Date.UTC(date1.getFullYear(), date1.getMonth(), date1.getDate());
+    const dateUTC2 = Date.UTC(date2.getFullYear(), date2.getMonth(), date2.getDate());
+
+    return Math.abs(Math.floor((dateUTC2 - dateUTC1) / msPeerDays));
   }
 }
